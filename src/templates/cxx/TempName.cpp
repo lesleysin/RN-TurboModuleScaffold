4dc268ee6c229118d2319f4957d7b6e61a71d3d5
Nikita Sinyavin <nsinyavin@denumpay.ru>
2023-02-06 23:09:33 +0200

    - Remove unused template files

--- src/templates/cxx/TempName.cpp
+++
@@ -1,9 +0,0 @@
-#include "TempName.h"
-
-namespace TempName
-{
-    double multiply(double num1, double num2)
-    {
-        return num1 * num2;
-    }
-}
--- src/templates/cxx/TempName.h
+++
@@ -1,9 +0,0 @@
-#ifndef TempName_H
-#define TempName_H
-
-namespace TempName
-{
-    double multiply(double num1, double num2);
-}
-
-#endif /* TempName_H */
